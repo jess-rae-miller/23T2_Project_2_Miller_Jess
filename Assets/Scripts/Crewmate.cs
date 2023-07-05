fcf11639170de97cdd31ca8f338836b1ed37a251
jess-rae-miller <136469171+jess-rae-miller@users.noreply.github.com>
2023-07-05 16:02:32 +1000

    script changes

--- Assets/Scripts/Crewmate.cs
+++ Assets/Scripts/Crewmate.cs
@@ -4,26 +4,28 @@ using UnityEngine;
 
 public class Crewmate : MonoBehaviour
 {
-    [SerializeField] public string name;
+    [SerializeField] public new string name;
     [SerializeField] public string hobby;
     [SerializeField] public bool isAlien;
 
     public int maxCrew = 10;
 
     //Human hobbies
-    private string[] humanHobbies = { "Reading", "Writing", "Painting", "Sport", "Cooking", "Film"};
+    private string[] humanHobbies = { "Reading", "Writing", "Painting", "Sport", "Cooking", "Film" };
     //Alien hobbies
-    private string[] alienHobbies = { "Assimilating", "Synthesizing", "Breathing", "Smelling", "Taxidermy", "Devouring"};
+    private string[] alienHobbies = { "Assimilating", "Synthesizing", "Breathing", "Smelling", "Taxidermy", "Fascism" };
+
+    private System.Random rnd = new System.Random();
+    //Human names
+    private string[] humanFirstNames = { "Emma", "Liam", "Olivia", "Noah", "Ava", "Isabella", "Sophia", "Mia", "Charlotte", "Amelia", "Harper", "Michael", "Jacob", "Emily", "Elizabeth", "Mila", "Ella", "Avery", "Sofia", "Camila" };
+    private string[] humanLastNames = { "Smith", "Johnson", "Williams", "Brown", "Jones", "Miller", "Davis", "Garcia", "Rodriguez", "Wilson", "Martinez", "Anderson", "Taylor", "Thomas", "Hernandez", "Moore", "Martin", "Jackson", "Thompson", "White" };
 
     // Start is called before the first frame update
-    void Start()
-    {
-        StartGame();
-    }
+    void Start() => StartGame();
 
     private void StartGame()
     {
-        if (maxCrew < 10);
+        while (maxCrew < 10)
         {
             GenerateCrewmate();
         }
@@ -31,7 +33,7 @@ public class Crewmate : MonoBehaviour
 
     private void GenerateCrewmate()
     {
-        string name = GenerateRandomName();
+        _ = GenerateRandomName();
         string hobby;
         // 1/4 chance of being an alien????
         bool isAlien = Random.value < 0.25f; 
@@ -52,10 +54,13 @@ public class Crewmate : MonoBehaviour
         return hobbies[randomIndex];
     }
 
-    private void GenerateRandomName()
+    private string GenerateRandomName()
     {
-        //???????
-       
+        if (isAlien)
+        {
+            string newName = humanFirstNames[rnd.Next(0, humanFirstNames.Length)] + humanLastNames[rnd.Next(0, maxValue: humanLastNames.Length)];
+            return newName;
+        }
     }
 
     // Update is called once per frame
